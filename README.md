523e64b5a5679108b017e220fbc999eb6cb6a386
kemora_13conf <Abdomouak48@gmail.com>
2024-02-29 16:32:15 +0100

    Initial commit. x_0!

---
+++ README.md
@@ -0,0 +1,3 @@
+# chatapp_flutter_1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/chatapp_flutter_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chatapp_flutter_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
